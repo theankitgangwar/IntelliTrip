0f28dd55c8ab757ffb1f8f5b12c290a0e1c8e429
theankitgangwar <theankitgangwar@gmail.com>
2025-10-05 01:58:16 +0530

    change model Gemini

--- src/service/AIModel.jsx
+++ src/service/AIModel.jsx
@@ -3,9 +3,10 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
   const apiKey = import.meta.env.VITE_GOOGLE_GEMINI_AI_API_KEY;
   const genAI = new GoogleGenerativeAI(apiKey);
   
-  const model = genAI.getGenerativeModel({
-    model: "gemini-1.5-flash", 
-  }); 
+ const model = genAI.getGenerativeModel({
+  model: "models/gemini-2.5-pro",
+});
+
   
   const generationConfig = {
     temperature: 1,
